b3c6a9c9222fafdc09f84a2979e962c19e81cf5f
Hirad Abbasi <hiradcana@gmail.com>
2021-04-11 17:37:05 -0500

    Showtimes added properly

--- js/maps.js
+++ js/maps.js
@@ -5,14 +5,16 @@ const baseURL = `https://api-gate2.movieglu.com/`;
 const moviedbBaseURL = 'https://api.themoviedb.org/3/';
 
 const container = document.querySelector('.titles-wrapper');
-const main = document.querySelector("main");
-const showTimeDate = document.querySelector(".showTimeDate");
+const main = document.querySelector('main');
+const showTimeDate = document.querySelector('.showTimeDate');
+const cinemasContainer = document.querySelector('.cinemas');
+const showTimesListContainer = document.querySelector('.showTimes-list');
 const weekDays = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+const allMonths = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
 let testArr = [];
-
+let testArr2 = [];
 
 // let map;
-
 // let options = {
 //   enableHighAccuracy: true,
 //   timeout: 7000,
@@ -21,7 +23,7 @@ let testArr = [];
 
 // function success(pos) {
 //   let crd = pos.coords;
-
+  
 //   mapboxgl.accessToken ="pk.eyJ1IjoiaGlyYWRhYmJhc2kiLCJhIjoiY2ttbWJjNzJqMDh3aDJ3bzQ4eXp6cWJjZCJ9.PB8mTgztoMbR3VCzuKUYfQ";
 //   var map = new mapboxgl.Map({
 //     container: "map",
@@ -29,7 +31,7 @@ let testArr = [];
 //     center: [-96, 37.8],
 //     zoom: 3,
 //   });
-
+  
 //   map.on("load", function () {
 //     map.loadImage("https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
 //       function (error, image) {
@@ -113,43 +115,87 @@ document.querySelector('.background').style.backgroundPosition = `center`;
 document.querySelector('.background').style.backgroundSize = `cover`;
 document.querySelector('.background').style.height = `100vh`;
 
-addTrailers();
-async function addTrailers() {
-  const response = await fetch(`${moviedbBaseURL}movie/${user.id}/videos?api_key=${moviedbAPI_KEY}&type="trailer"&language=en-US`);
-  const JSON = await response.json();
-  main.insertAdjacentHTML('beforeend',`<iframe width="100%" height=750px" src="https://www.youtube.com/embed/${JSON.results[0].key}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
-}
+// addTrailers();
+// async function addTrailers() {
+//   const response = await fetch(`${moviedbBaseURL}movie/${user.id}/videos?api_key=${moviedbAPI_KEY}&type="trailer"&language=en-US`);
+//   const JSON = await response.json();
+//   main.insertAdjacentHTML('beforeend',`<iframe width="100%" height=750px" src="https://www.youtube.com/embed/${JSON.results[0].key}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`)
+// }
 
 getAllShowTimes(user.testID);
 async function getAllShowTimes(id) {
   const response = await fetch(`https://api.internationalshowtimes.com/v4/showtimes?city_name=winnipeg&movie_id=${id}${API_KEY}`);
   const JSON = await response.json();
-  console.log(JSON.showtimes.sort((a, b) => new Date(a.start_at).getDate() - new Date(b.start_at).getDate()));
+  JSON.showtimes.sort((a, b) => new Date(a.start_at).getDate() - new Date(b.start_at).getDate());
   
   JSON.showtimes.forEach(showTime => {
     if(!testArr.includes(new Date(showTime.start_at).getDate())){
-      testArr.push(new Date(showTime.start_at).getDate())
+      testArr.push(new Date(showTime.start_at).getDate());
+      testArr2.push(new Date(showTime.start_at).getMonth())
     }
   });
+
+  for (let i = 0; i < testArr.length; i++) {
+    showTimeDate.insertAdjacentHTML('beforeend', `
+      <div class="showTime">
+        <h3 class="showTimeDate">${testArr[i]}</h3>
+        <h3 class="showTimeMonth">${allMonths[testArr2[i]]}</h3>
+      </div>
+    `)
+  }
   
-  // testArr.forEach(element => {
-  //   showTimeDate.insertAdjacentHTML('beforeend', `
-  //     <div class="showTime">${element}</div>
-  //   `)
-  // });
-  
-  // showTimeDate.onclick = e => {
+  let newItems = [];
+  showTimeDate.onclick = e => {
+    const date = e.target.closest('.showTime');
     
-  // }
+    if (date !== null) {
+      newItems = [];
+      cinemasContainer.textContent = '';
+      JSON.showtimes.forEach(showTime => {
+        if(new Date(showTime.start_at).getDate() == date.querySelector('.showTimeDate').textContent){
+          if(!newItems.includes(showTime.cinema_id)) {
+            newItems.push(showTime.cinema_id);
+          }
+        }
+      });      
+    }
+    testFunction(date.querySelector('.showTimeDate').textContent);
+  }
   
-  // popularMovies.onclick = e => {
-  //   const movie = e.target.closest('.movie')
-  
-  //   if (movie !== null) {
-  //     getMovieReviews(movie.dataset.movieId);
-  //   }
-  // }
-  //console.log(daysArray);
+  function testFunction(date) {
+    newItems.forEach(async element => {
+      await testyIRES(element);
+      JSON.showtimes.forEach(showTime => {
+        if(showTime.cinema_id == element && new Date(showTime.start_at).getDate() == date){
+          console.log(new Date(showTime.start_at).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+          document.getElementById(`${showTime.cinema_id}`).insertAdjacentHTML('afterbegin', `
+            <div class="showTimes">${new Date(showTime.start_at).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</div>
+          `);
+        } 
+      });
+    });
+  }
+
+async function testyIRES(cinemaID) {  
+  const response = await fetch(`https://api.internationalshowtimes.com/v4/cinemas?cinema_id=${cinemaID}${API_KEY}`);
+  const JSON = await response.json();
+  JSON.cinemas.forEach(element => {
+    if(element.id === cinemaID){
+     console.log(element.name, element.location.address.display_text);
+     cinemasContainer.insertAdjacentHTML('beforeend', `
+     <div class="cinema-details">
+        <h2>${element.name}</h2>
+        <h4>${element.location.address.display_text}</h4>
+        <h5><i class="fas fa-map-marker-alt"></i> 1.9 km</h5> 
+        <div id="map" class="mapboxgl-map" style='width: 800px; margin:0 auto; '>
+          <div id='map' style='width: 100%; height:500px;'></div>
+        </div>
+        <div class="showTimes-list" id="${element.id}">
+        </div>
+      </div>`)
+    }
+  });
+}
   
   // daysArray.forEach(element => {
   //   console.log(weekDays[element]);    
